bb4adb7569edbdc1288354eac6a68740fa1b1429
Vishal Chavhan <72196647+Vishalc2901@users.noreply.github.com>
2023-05-20 18:09:52 +0530

    Add files via upload

---
+++ Programming Task - 3 (Python Maths)/FindAngleMBC.py
@@ -0,0 +1,10 @@
+import math
+AB = int(input())
+BC = int(input())
+H = math.sqrt(AB**2 + BC**2)
+H = H/2.0
+adj = BC/2.0
+Output = int(round(math.degrees(math.acos(adj/H))))
+Output=str(Output)
+degree_sign = u"\N{DEGREE SIGN}"
+print(Output+degree_sign)
